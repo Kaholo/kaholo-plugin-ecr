c8fa429a91a891d22284de2e8f3884ba171dc2e9
Kacper Debowski <kacper@kaholo.io>
2022-02-07 15:21:34 +0100

    Added dynamic regions and fix access key type to vault (KP-494)

--- autocomplete.js
+++ autocomplete.js
@@ -1,92 +1,52 @@
-async function listRegions(query, pluginSettings) {
-    let options = [
-      {
-        "id":"us-east-1",
-        "value":"US East (N. Virginia)"
-      },
-      {
-        "id":"us-east-2",
-        "value":"US East (Ohio)"
-      },
-      {
-        "id":"us-west-1",
-        "value":"US West (N. California)"
-      },
-      {
-        "id":"us-west-2",
-        "value":"US West (Oregon)"
-      },
-      {
-        "id":"ap-east1",
-        "value":"Asia Pacific (Hong Kong)"
-      },
-      {
-        "id":"ap-south1",
-        "value":"Asia Pacific (Mumbai)"
-      },
-      {
-        "id":"ap-southeast-1",
-        "value":"Asia Pacific (Singapore)"
-      },
-      {
-        "id":"ap-southeast-2",
-        "value":"Asia Pacific (Sydney)"
-      },
-      {
-        "id":"ap-northeast-1",
-        "value":"Asia Pacific (Tokyo)"
-      },
-      {
-        "id":"ap-northeast-2",
-        "value":"Asia Pacific (Seoul)"
-      },
-      {
-        "id":"sa-east-1",
-        "value":"South America (São Paulo)"
-      },
-      {
-        "id":"eu-central-1",
-        "value":"EU (Frankfurt)"
-      },
-      {
-        "id":"eu-west-1",
-        "value":"EU (Ireland)"
-      },
-      {
-        "id":"eu-west-2",
-        "value":"EU (London)"
-      },
-      {
-        "id":"eu-west-3",
-        "value":"EU (Paris)"
-      },
-      {
-        "id":"eu-north-3",
-        "value":"EU (Stockholm)"
-      },
-      {
-        "id":"eu-south-1",
-        "value":"EU (Milan)"
-      },
-      {
-        "id":"ca-central-1",
-        "value":"Canada (Central)"
-      },
-      {
-        "id":"me-south-1",
-        "value":"Middle East (Baharain)"
-      },
-      {
-        "id":"af-south-1",
-        "value":"Africa (Cape Town)"
-      }
-    ]
-    //let options = response.items.map((item) => ({ id: item.id, value: item.name}));
-    if (!query) {
-        return options;
+const {getEc2, getLightsail} = require("./helpers");
+
+const MISSING_OR_INCORRECT_CREDENTIALS_ERROR = "Missing or incorrect credentials - please select valid access and secret keys first";
+
+function paramsMapper(pluginSettings,actionParams){
+    const settings = {};
+    const params = {};
+
+    if (pluginSettings && pluginSettings.length > 0) {
+        pluginSettings.forEach(setting=>{
+            settings[setting.name] = setting.value;
+        })
     }
-    const filteredList = options.filter(val => val.value.includes(query))
-    return filteredList;
+
+    if (actionParams && actionParams.length > 0) {
+        actionParams.forEach(param=>{
+            params[param.name] = param.value;
+        })
+    }
+
+    return {settings, params};
+}
+
+async function listRegions(query, pluginSettings, actionParams) {
+    let { settings, params } = paramsMapper(pluginSettings, actionParams);
+    params = {...params, region: params.region || "eu-west-2"};
+    const ec2 = getEc2(params, settings);
+    const lightsail = getLightsail(params, settings);
+
+    const ec2RegionsPromise = ec2.describeRegions().promise();
+    const lightsailRegionsPromise = lightsail.getRegions().promise();
+
+    return Promise.all([ec2RegionsPromise, lightsailRegionsPromise]).then(
+        ([ec2Regions, lightsailRegions]) => {
+            return ec2Regions.Regions.map((ec2Region) => {
+                const lsRegion = lightsailRegions.regions.find((x) => x.name === ec2Region.RegionName);
+                return lsRegion ?
+                    { id: ec2Region.RegionName, value: `${ec2Region.RegionName} (${lsRegion.displayName})` } :
+                    { id: ec2Region.RegionName, value: ec2Region.RegionName }
+            }).sort((a,b) => {
+                if (a.value > b.value) return 1;
+                if (a.value < b.value) return -1;
+                return 0;
+            });
+        }
+    ).catch((err) => {
+        console.error(err);
+        throw MISSING_OR_INCORRECT_CREDENTIALS_ERROR;
+    });
 }
 
 
--- helpers.js
+++ helpers.js
@@ -9,36 +9,54 @@ function getEcr(action, settings) {
     });
 }
 
-async function executeCmd(command){
-	return new Promise((resolve,reject) => {
-		child_process.exec(command, (error, stdout, stderr) => {
-			if (error) {
-			   return reject(error);
-			}
-			if (stderr) {
-				console.log(`stderr: ${stderr}`);
-			}
-			return resolve(stdout);
-		});
-	})
+function getEc2(params, settings) {
+    return new aws.EC2({
+        region: handleAutoComplete(params.region),
+        accessKeyId: params.accessKeyId || settings.accessKeyId,
+        secretAccessKey: params.secretAccessKey || settings.secretAccessKey
+    });
 }
 
-function operationCallback(resolve,reject) {
-    return function(err,result){
+function getLightsail(params, settings) {
+    return new aws.Lightsail({
+        region: handleAutoComplete(params.region),
+        accessKeyId: params.accessKeyId || settings.accessKeyId,
+        secretAccessKey: params.secretAccessKey || settings.secretAccessKey
+    });
+}
+
+async function executeCmd(command) {
+    return new Promise((resolve, reject) => {
+        child_process.exec(command, (error, stdout, stderr) => {
+            if (error) {
+                return reject(error);
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+            }
+            return resolve(stdout);
+        });
+    })
+}
+
+function operationCallback(resolve, reject) {
+    return function (err, result) {
         if (err) reject(err);
         else resolve(result);
     }
 }
 
-function handleAutoComplete(param){
-	if (param && typeof(param) === "object" && param.hasOwnProperty("id")){
-		return param.id;
-	}
-	return param;
+function handleAutoComplete(param) {
+    if (param && typeof (param) === "object" && param.hasOwnProperty("id")) {
+        return param.id;
+    }
+    return param;
 }
 
 module.exports = {
     getEcr,
+    getEc2,
+    getLightsail,
     executeCmd,
     operationCallback
 };
